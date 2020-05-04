a70e839c5052ba3f3d9391c40863d65c8d3d882f
Joel Lyons <Joel.lyons@outlook.com>
2020-05-03 21:57:48 -0500

    Added 'isOpen' state to nav component.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './Home.css';
 import Navigation from '../Navigation';
 
-function Home() {
-    return (
-        <div>
-            <Navigation />
-            <h1>Joel Lyons - This is my Home Component!</h1>
-        </div>
-    );
+class Home extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Navigation />
+                <h1>Joel Lyons - This is my Home Component!</h1>
+                
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-function Navigation() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
-    );
+class Navigation extends React.Component {
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        return (
+            <div>
+                <img src="menu_hamburger.png" alt="hamburger navigation icon" id="nav-hamburger" onClick={() => this.setState({isOpen: !this.state.isOpen})} />
+                <nav className={this.state.isOpen ? "show" : "hidden"}>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+    
 }
 
-export default Navigation;
+export default Navigation
