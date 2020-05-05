b6daa460c4ff46a5dce70f1bd4ebc129b673d366
Joel Lyons <joel.lyons@outlook.com>
2020-05-04 21:53:28 -0500

    Initial Home Commit.

--- src/App.route.js
+++ src/App.route.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import Home from './components/Home';
-import App from './App';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 class AppRoute extends Component {
     render() {
         return (
             <Route path="/" component={Home}>
                 <Route exact path="/" component={Home} />
-                <Route path="/about" component={Home} />
-                <Route path="/skills" component={Home} />
-                <Route path="/projects" component={Home} />
-                <Route path="/contact" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/skills" component={Skills} />
+                <Route path="/projects" component={Projects} />
+                <Route path="/contact" component={Contact} />
             </Route>
         );
     }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.css';
+import Navigation from '../Navigation';
+
+class Contact extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Navigation />
+                <h1>Under Construction...</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default Contact;
