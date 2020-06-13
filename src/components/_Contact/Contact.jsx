b4ecd7fb62717de6b2425bf02407c92af94fa08e
Joel Lyons <joel.lyons@outlook.com>
2020-06-12 23:20:24 -0500

    Added react-bootstrap (v4.5). Updated site to work with bootstrap css styles. Created contact form using react-bootstrap components.

--- src/components/_Contact/Contact.jsx
+++ src/components/_Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Contact.css';
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
+import {Form, Button} from 'react-bootstrap';
 
 class Contact extends React.Component {
     render() {
@@ -9,25 +10,25 @@ class Contact extends React.Component {
             <div className="app">
                 <Navigation />
                 <div id="contact-wrapper">
-                    <div className="contact">
+                    <Form className="contact">
                         <h1>Contact</h1>
-                        <hr/>
-                        <div className="name">
-                            <label for="name">Name</label><br/>
-                            <input type="text" name="name" id="name" />
-                        </div>
-                        <div className="subject">
-                            <label for="subject">Subject</label><br/>
-                            <input type="text" name="subject" id="subject" />
-                        </div>
-                        <div className="body">
-                            <label for="body">Message</label><br/>
-                            <textarea rows="10" cols="50" name="body" id="body"></textarea>
-                        </div>
-                        <div className="submit">
-                            <button id="submit" name="submit">Send</button>
-                        </div>
-                    </div>
+
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Joel Lyons" />
+
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder="email@example.com" />
+
+                        <Form.Label>Subject</Form.Label>
+                        <Form.Control type="text" placeholder="RE: something?" />
+
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+
+                        <Button type="submit" placeholder="Type your message here..." className="float-right">
+                            Send
+                        </Button>
+                    </Form>
                 </div>
                 <Footer />
             </div>
